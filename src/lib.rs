80660b84d19812fad3bb6a3aacb70f81339c3c2e
WizardPlatypus <f.volik.m@gmail.com>
2024-09-28 16:47:30 +0300

    Gauss's algorithm is online and running!
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,16 @@ pub struct Matrix<T> {
 }
 
 impl<T> Matrix<T> {
+    pub fn new<F: Fn(usize, usize) -> T>(rows: usize, cols: usize, f: F) -> Matrix<T> {
+        let mut data = Vec::with_capacity(rows * cols);
+        for row in 0..rows {
+            for col in 0..cols {
+                data.push(f(row, col));
+            }
+        }
+        Matrix { rows, cols, data }
+    }
+
     fn mapped(&self, row: usize, col: usize) -> usize {
         row * self.cols + col
     }
@@ -34,6 +44,28 @@ impl<T> Matrix<T> {
     }
 }
 
+impl<T: std::fmt::Debug> std::fmt::Debug for Matrix<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use std::fmt::Write;
+
+        let max = self
+            .data
+            .iter()
+            .map(|t| format!("{t:?}").len())
+            .max()
+            .unwrap();
+
+        for row in 0..self.rows {
+            f.write_char('|')?;
+            for col in 0..self.cols {
+                write!(f, " {:>width$?}", self[(row, col)], width = max)?;
+            }
+            f.write_str(" |\n")?;
+        }
+        Ok(())
+    }
+}
+
 impl<T: PartialOrd> Matrix<T> {
     pub fn max_in_row(&self, row: usize, skip: Option<usize>) -> usize {
         let mut max = skip.unwrap_or(0);
@@ -42,7 +74,7 @@ impl<T: PartialOrd> Matrix<T> {
                 max = col;
             }
         }
-	max
+        max
     }
 
     pub fn max_in_col(&self, col: usize, skip: Option<usize>) -> usize {
@@ -52,7 +84,7 @@ impl<T: PartialOrd> Matrix<T> {
                 max = row;
             }
         }
-	max
+        max
     }
 }
 
@@ -73,6 +105,21 @@ impl<T> std::ops::IndexMut<(usize, usize)> for Matrix<T> {
     }
 }
 
+impl<T> FromIterator<T> for Matrix<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let data: Vec<T> = iter.into_iter().collect();
+        let mut n = 1;
+        while n * (n + 1) <= data.len() {
+            n += 1;
+        }
+        Matrix {
+            rows: n - 1,
+            cols: n,
+            data,
+        }
+    }
+}
+
 pub trait Gauss {
     fn max_in_row(&self, row: usize) -> usize;
     fn max_in_column(&self, col: usize) -> usize;
@@ -83,50 +130,58 @@ pub trait Gauss {
 
 impl Gauss for Matrix<f64> {
     fn max_in_row(&self, row: usize) -> usize {
-	self.max_in_row(row, Some(row))
+        self.max_in_row(row, Some(row))
     }
 
     fn max_in_column(&self, col: usize) -> usize {
-	self.max_in_col(col, Some(col))
+        self.max_in_col(col, Some(col))
     }
-    
+
     fn modify(&mut self, step: usize) {
         let first = 1.0 / self[(step, step)];
-        for col in step..self.rows {
+        for col in step..self.cols {
             self[(step, col)] *= first;
         }
 
-        for row in (step + 1)..self.cols {
+        for row in (step + 1)..self.rows {
             let first = self[(row, step)];
-            for col in step..self.rows {
+            for col in step..self.cols {
                 self[(row, col)] -= self[(step, col)] * first;
             }
         }
     }
-    
+
     fn reverse(&self) -> Vec<f64> {
-	let mut x = vec![0.0; self.rows];
-	for k in 1..=self.rows {
-	    let i = self.rows - k;
-	    x[i] = self[(i, self.cols - 1)];
-	    for j in (i + 1)..self.rows {
-		x[i] -= self[(i, j)];
-	    }
-	}
-	x
+        let mut x = vec![0.0; self.rows];
+        for k in 1..=self.rows {
+            let i = self.rows - k;
+            x[i] = self[(i, self.cols - 1)];
+            for j in (i + 1)..self.rows {
+                x[i] -= self[(i, j)];
+            }
+        }
+        x
     }
-    
+
     fn solve(&mut self) -> f64 {
-	let mut det = 1.0;
-	for step in 0..self.rows {
-	    let main= self.max_in_column(step);
-	    det *= self[(main, step)];
-	    if main != step {
-		self.swap_rows(main, step);
-		det *= -1.0;
-	    }
-	    self.modify(step);
-	}
-	det
+        let mut det = 1.0;
+        for step in 0..self.rows {
+	    log::debug!("Step {step}");
+            let main = self.max_in_column(step);
+	    log::debug!("Main ({main}, {step}) = {}", self[(main, step)]);
+            det *= self[(main, step)];
+            if main != step {
+		log::debug!("Swapping rows {main} and {step}");
+                self.swap_rows(main, step);
+		log::debug!("After swap:");
+		log::debug!("\n{:?}", self);
+                det *= -1.0;
+            }
+	    log::debug!("Applying M{step}");
+            self.modify(step);
+	    log::debug!("After M{step}:");
+	    log::debug!("\n{:?}", self);
+        }
+        det
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use matrices::*;
+
+fn my_matrix(n: usize, i: usize, j: usize) -> i64 {
+    if i == 0 && j == 0 {
+        1
+    } else if i == j {
+        0
+    } else if j == n {
+        (i + 1) as i64
+    } else if i > j {
+        -((j + 1) as i64)
+    } else {
+        // if j > i
+        (j + 1) as i64
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    let n = 5;
+    let mut m = Matrix::new(n, n + 1, |i, j| my_matrix(n, i, j) as f64);
+    log::debug!("\n{:?}", m);
+
+    let det = <Matrix<f64> as Gauss>::solve(&mut m);
+    let x = <Matrix<f64> as Gauss>::reverse(&m);
+    log::debug!("\n{:?}", m);
+    log::debug!("{:?}", det);
+    log::debug!("{:?}", x);
 }
