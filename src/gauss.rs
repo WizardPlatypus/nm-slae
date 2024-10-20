dc7b8a263c773a8b029224d3a222780cb4a10308
WizardPlatypus <f.volik.m@gmail.com>
2024-10-20 10:49:46 +0300

    Implementex calc_l and calc_u

--- src/gauss.rs
+++ src/gauss.rs
@@ -1,3 +1,84 @@
+use crate::Matrix;
+use crate::Iteratable;
+
+fn total_cmp(a: &(usize, f64), b: &(usize, f64)) -> std::cmp::Ordering {
+    f64::total_cmp(&a.1, &b.1)
+}
+
+pub fn calc_l<M: Matrix<Item=f64>>(m: &mut M) -> Option<()> {
+    let h: usize = m.height();
+    let w: usize = m.width();
+
+    for i in 0..h {
+        let diag = *m.at(i, i)?;
+        let value = if diag == 0.0 {
+            let (lead, value) = m
+                .column(i)
+                .map(Clone::clone)
+                .enumerate()
+                .skip(i)
+                .max_by(total_cmp)?;
+
+            if lead != i {
+                m.swap_rows(lead, i)?;
+            }
+
+            value
+        } else {
+            diag
+        };
+
+        for row in (i + 1)..h {
+            let factor = m.at(row, i)? / value;
+            *m.at_mut(row, i)? = 0.0;
+            for column in (i + 1)..w {
+                *m.at_mut(row, column)? -= m.at(i, column)? * factor;
+            }
+        }
+    }
+    Some(())
+}
+
+pub fn calc_u<M: Matrix<Item=f64>>(m: &mut M) -> Option<()> {
+    let h: usize = m.height();
+    let w: usize = m.width();
+
+    for i in 0..h {
+        let t = h - i - 1;
+        let diag = *m.at(t, t)?;
+        let value = if diag == 0.0 {
+            let (lead, value) = m
+                .column(t)
+                .map(Clone::clone)
+                .enumerate()
+                .rev()
+                .skip(i)
+                .max_by(total_cmp)?;
+
+            if lead != t {
+                m.swap_rows(lead, t)?;
+            }
+
+            value
+        } else {
+            diag
+        };
+
+        for row in (i + 1)..h {
+            let row = h - row - 1;
+            let factor = m.at(row, t)? / value;
+            *m.at_mut(row, t)? = 0.0;
+            for column in row..t {
+                *m.at_mut(row, column)? -= m.at(t, column)? * factor;
+            }
+            for column in (t + 1)..w {
+                *m.at_mut(row, column)? -= m.at(t, column)? * factor;
+            }
+        }
+    }
+    Some(())
+}
+/*
 use super::{Matrix};
 
 pub struct Gauss {
@@ -202,3 +283,5 @@ impl std::fmt::Display for State {
         Ok(())
     }
 }
+
+// */
