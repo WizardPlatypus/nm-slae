6e0e7dde14b61d0e29240fa91c5b46e97e73d5bb
WizardPlatypus <f.volik.m@gmail.com>
2024-10-20 12:02:22 +0300

    Trying on indexed iterators

--- src/gauss.rs
+++ src/gauss.rs
@@ -1,5 +1,4 @@
-use crate::Matrix;
-use crate::Iteratable;
+use crate::{Matrix, Iteratable, Indexable};
 
 fn total_cmp(a: &(usize, f64), b: &(usize, f64)) -> std::cmp::Ordering {
     f64::total_cmp(&a.1, &b.1)
@@ -27,6 +26,17 @@ pub fn calc_l<M: Matrix<Item=f64>>(m: &mut M) -> Option<()> {
         } else {
             diag
         };
+
+        /* less readable
+        for mut row in m.irows().skip(i + 1) {
+            let (r, c) = row.next()?;
+            let factor = m.at(r, c)? / value;
+            *m.at_mut(r, c)? = 0.0;
+            for (r, c) in row {
+                *m.at_mut(r, c)? -= m.at(i, c)? * factor;
+            }
+        }
+        // */
 
         for row in (i + 1)..h {
             let factor = m.at(row, i)? / value;
@@ -64,6 +74,24 @@ pub fn calc_u<M: Matrix<Item=f64>>(m: &mut M) -> Option<()> {
             diag
         };
 
+        /* less readable
+        for (j, row) in m.irows().enumerate().rev().skip(i + 1) {
+            let factor = m.at(j, t)? / value;
+            *m.at_mut(j, t)? = 0.0;
+
+            let row = row.skip(j + 1);
+            for (r, c) in row.clone().take(t - j - 1) {
+                *m.at_mut(r, c)? -= m.at(i, c)? * factor;
+            }
+
+            let row = row.skip(1);
+            for (r, c) in row {
+                *m.at_mut(r, c)? -= m.at(i, c)? * factor;
+            }
+        }
+        // */
+
+
         for row in (i + 1)..h {
             let row = h - row - 1;
             let factor = m.at(row, t)? / value;
@@ -79,129 +107,6 @@ pub fn calc_u<M: Matrix<Item=f64>>(m: &mut M) -> Option<()> {
     Some(())
 }
 /*
-use super::{Matrix};
-
-pub struct Gauss {
-    a: Matrix<f64>,
-    x: Option<Vec<f64>>,
-    det: Option<f64>,
-    trace: Vec<State>,
-}
-
-pub enum State {
-    Created {
-        matrix: Matrix<f64>,
-    },
-    Main {
-        iter: usize,
-        row: usize,
-        column: usize,
-        value: f64,
-    },
-    Swapped {
-        iter: usize,
-        a: usize,
-        b: usize,
-        n: usize,
-    },
-    Modified {
-        iter: usize,
-        matrix: Matrix<f64>,
-    },
-    Solved {
-        x: Vec<f64>,
-        det: f64,
-    },
-}
-
-impl Gauss {
-    fn at(&self, row: usize, col: usize) -> &f64 {
-        self.a.at(row, col)
-    }
-
-    fn at_mut(&mut self, row: usize, col: usize) -> &mut f64 {
-        self.a.at_mut(row, col)
-    }
-
-    fn log(&mut self, state: State) {
-        self.trace.push(state)
-    }
-
-    pub fn unbox(self) -> (Matrix<f64>, Option<Vec<f64>>, Option<f64>, Vec<State>) {
-        let Gauss { a, x, det, trace } = self;
-        (a, x, det, trace)
-    }
-}
-
-impl Gauss {
-    fn modify(&mut self, iter: usize) {
-        let first = 1.0 / self.at(iter, iter);
-        for col in iter..self.a.width {
-            *self.at_mut(iter, col) *= first;
-        }
-
-        for row in (iter + 1)..self.a.height {
-            let first = *self.at(row, iter);
-            for col in iter..self.a.width {
-                *self.at_mut(row, col) -= self.at(iter, col) * first;
-            }
-        }
-    }
-
-    fn next(&mut self, iter: usize) {
-        let main = self
-            .a
-            .column(iter)
-            .enumerate()
-            .skip(iter)
-            .max_by(|&(_, a), &(_, b)| f64::total_cmp(a, b));
-        if main.is_none() {
-            return;
-        }
-        let (main, &value) = main.unwrap();
-
-        self.log(State::Main {
-            iter,
-            row: main,
-            column: iter,
-            value,
-        });
-
-        if main != iter {
-            self.a.swap_rows(main, iter);
-            self.log(State::Swapped {
-                iter,
-                a: main,
-                b: iter,
-                n: self.a.height(),
-            });
-        }
-
-        self.modify(iter);
-        self.log(State::Modified {
-            iter,
-            matrix: self.a.clone(),
-        });
-    }
-
-    fn forward_pass(&mut self) {
-        for iter in 0..self.a.height() {
-            self.next(iter);
-        }
-    }
-
-    fn backward_pass(&self) -> Vec<f64> {
-        let mut x = vec![0.0; self.a.height];
-        for k in 1..=self.a.height {
-            let i = self.a.height - k;
-            x[i] = *self.at(i, self.a.width - 1);
-            for j in (i + 1)..self.a.height {
-                x[i] -= self.at(i, j);
-            }
-        }
-        x
-    }
-
     pub fn solve(&mut self) {
         self.forward_pass();
 
@@ -213,9 +118,7 @@ impl Gauss {
 
         self.log(State::Solved { x, det });
     }
-}
 
-impl Gauss {
     fn det(&self) -> f64 {
         let mut det = 1.0;
         for state in self.trace.iter() {
@@ -238,50 +141,4 @@ impl Gauss {
         det
     }
 }
-
-impl TryFrom<Matrix<f64>> for Gauss {
-    type Error = Matrix<f64>;
-    fn try_from(value: Matrix<f64>) -> Result<Self, Self::Error> {
-        if value.height() + 1 != value.width() {
-            Err(value)
-        } else {
-            Ok(Gauss {
-                a: value.clone(),
-                x: None,
-                det: None,
-                trace: vec![State::Created { matrix: value }],
-            })
-        }
-    }
-}
-
-use std::fmt::Write;
-impl std::fmt::Display for State {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            State::Created { matrix } => {
-                writeln!(f, "A =\n{:.2}", matrix)?;
-            }
-            State::Main {
-                iter,
-                row,
-                column,
-                value,
-            } => {
-                writeln!(f, "a{iter} = A[{row},{column}] = {value:.2}")?;
-            }
-            State::Swapped { iter, a, b, n: _n } => {
-                writeln!(f, "P{iter} = E{{{a}, {b}}}")?;
-            }
-            State::Modified { iter, matrix } => {
-                writeln!(f, "A{iter} =\n{:.2}", matrix)?;
-            }
-            State::Solved { x, det } => {
-                writeln!(f, "X = {:.2?}, det = {:.2}", x, det)?;
-            }
-        }
-        Ok(())
-    }
-}
-
 // */
